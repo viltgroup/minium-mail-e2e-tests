85c5e678fe910c05a32c23d985a26f08768e03d0
Rui Figueira <rui.figueira@gmail.com>
2016-08-11 11:13:07 +0100

    Interaction listeners moved to based expression instead of browser

--- src/test/resources/steps/world.js
+++ src/test/resources/steps/world.js
@@ -1,19 +1,21 @@
 var timeUnits = require("minium/timeunits");
 
 var base;
-var loadingUnexistenceListener;
 
 World(function () {
 
   var loading = $(".loading").withCss("display", "block");
 
-  // base expression
-  base = $(":root").unless(".modal-backdrop").add(".modal-dialog");
-
   // we keep a variable for our interaction listener so that we can remove it at the end of the execution
-  loadingUnexistenceListener = minium.interactionListeners
+  var loadingUnexistenceListener = minium.interactionListeners
     .ensureUnexistence(loading)
     .withWaitingPreset("slow");
+
+  // base expression
+  base = $(":root")
+    .unless(".modal-backdrop")
+    .add(".modal-dialog")
+    .with(loadingUnexistenceListener);
 
   // browser configuration
   browser.configure()
@@ -22,18 +24,8 @@ World(function () {
     .done()
     .waitingPreset("slow")
       .timeout(10, timeUnits.SECONDS)
-    .done()
-    .interactionListeners()
-      .add(loadingUnexistenceListener)
     .done();
 
 }, function () {
-  // we need to remove our interaction listeners at the end so that in the next scenario we don't end up
-  // having two identical interaction listeners
-  browser.configure()
-    .interactionListeners()
-      .remove(loadingUnexistenceListener)
-    .done();
-
   base = undefined;
 });
